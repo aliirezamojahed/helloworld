12d51ac41552a4918b3da88d9a8a351f45715766
AlIrEzA <dl7.blogfa@gmail.com>
2020-08-01 22:52:49 +0430

    Changing pages.view, Clearing extra empty line between h1 and h2

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home_page_view(request):
-    return HttpResponse('<h1>Hello, World!<h1><br><h2>I\'m working with Django!</h2>')
+    return HttpResponse('<h1>Hello, World!<h1><h2>I\'m working with Django!</h2>')
